636247764b81ba92ac2b7073bf03282cb737b4eb
Mike Meisinger <mike.meisinger@gmail.com>
2016-08-05 02:08:22 -0400

    added react hot loader

--- server/index.js
+++ server/index.js
@@ -2,12 +2,20 @@
 import express from 'express'
 import path from 'path'
 
+import config from '../webpack.config.dev'
 import webpack from 'webpack'
 import webpackMiddleware from 'webpack-dev-middleware'
-import webpackConfig from '../webpack.config.dev'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 
 const app = express()
-app.use(webpackMiddleware(webpack(webpackConfig)))
+const compiler = webpack(config)
+
+app.use(webpackHotMiddleware(compiler))
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,21 +1,31 @@
 
+import webpack from 'webpack'
 import path from 'path'
 
 export default {
   devtools: 'eval-source-map',
-  entry: path.join(__dirname, '/source/index.js'),
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/source/index.js')
+  ],
   output: {
-    path: '/'
+    path: '/',
+    publicPath: '/'
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'source'),
-        loaders: [ 'babel' ]
+        loaders: [ 'react-hot', 'babel' ]
       }
     ]
   },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   resolve: {
     extensions: ['', '.js']
   }
